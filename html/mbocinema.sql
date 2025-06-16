5815c0d4b424c5c9638b5683387d85cc12dd4c14
TijsZ <tijs@vreijling.nl>
2025-06-16 13:42:49 +0200

    added films colum
    want die bestond nog niet
    nu werkt de code wel

--- html/mbocinema.sql
+++ html/mbocinema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Gegenereerd op: 02 jun 2025 om 13:05
+-- Gegenereerd op: 16 jun 2025 om 13:40
 -- Serverversie: 10.4.32-MariaDB
 -- PHP-versie: 8.2.12
 
@@ -24,6 +24,58 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Tabelstructuur voor tabel `movies`
+--
+
+CREATE TABLE `movies` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(255) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `room` int(11) DEFAULT NULL,
+  `seats` int(11) DEFAULT NULL,
+  `foto_url` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `movies`
+--
+
+INSERT INTO `movies` (`id`, `naam`, `rating`, `room`, `seats`, `foto_url`) VALUES
+(1, 'film', 10, 2, 52, 'logo.png'),
+(2, 'movie', 1, 0, 4, 'banner.png');
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `reserveringen`
+--
+
+CREATE TABLE `reserveringen` (
+  `id` int(11) NOT NULL,
+  `naam` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `telefoon` varchar(20) DEFAULT NULL,
+  `locatie` varchar(100) DEFAULT NULL,
+  `datum` date DEFAULT NULL,
+  `tijd` time DEFAULT NULL,
+  `aantal` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `movie_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `bevestigingsnummer` varchar(20) DEFAULT NULL,
+  `film` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Gegevens worden geëxporteerd voor tabel `reserveringen`
+--
+
+INSERT INTO `reserveringen` (`id`, `naam`, `email`, `telefoon`, `locatie`, `datum`, `tijd`, `aantal`, `created_at`, `movie_id`, `user_id`, `bevestigingsnummer`, `film`) VALUES
+(1, '1', '6002193@mborijnland.nl', '1', '1', '0000-00-00', '03:33:00', 3, '2025-06-16 11:38:41', NULL, 1, 'IBCX763GKT', 'film');
+
+-- --------------------------------------------------------
+
+--
 -- Tabelstructuur voor tabel `users`
 --
 
@@ -40,11 +92,25 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`ID`, `UserName`, `UserPassword`, `email`, `telefoonnumer`) VALUES
-(1, 'Tijs', '$2y$10$H.yVJRX2t3K9YzvBXHHC8e2Sqp01/wFKPTHrbOzthDsNrTSS6DABG', NULL, NULL);
+(1, 'Tijs', '$2y$10$H.yVJRX2t3K9YzvBXHHC8e2Sqp01/wFKPTHrbOzthDsNrTSS6DABG', NULL, 'Tijs'),
+(4, 'test', '$2y$10$gkFZugKOsfVHjxuzT5uSCOFoiqHzIMq45GTanR0vIBXHCWw1ur/mm', 'test@test.nl', '123123');
 
 --
 -- Indexen voor geëxporteerde tabellen
 --
+
+--
+-- Indexen voor tabel `movies`
+--
+ALTER TABLE `movies`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexen voor tabel `reserveringen`
+--
+ALTER TABLE `reserveringen`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `movie_id` (`movie_id`);
 
 --
 -- Indexen voor tabel `users`
@@ -58,39 +124,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT voor een tabel `movies`
+--
+ALTER TABLE `movies`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT voor een tabel `reserveringen`
+--
+ALTER TABLE `reserveringen`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT voor een tabel `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Beperkingen voor geëxporteerde tabellen
+--
+
+--
+-- Beperkingen voor tabel `reserveringen`
+--
+ALTER TABLE `reserveringen`
+  ADD CONSTRAINT `reserveringen_ibfk_1` FOREIGN KEY (`movie_id`) REFERENCES `movies` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
--- voor de 2 tables voor reserveringen en films
-
-CREATE TABLE movies (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    naam VARCHAR(255),
-    rating INT(11),
-    room INT(11),
-    seats INT(11),
-    foto_url TEXT
-);
-
-CREATE TABLE reserveringen (
-    id INT(11) AUTO_INCREMENT PRIMARY KEY,
-    naam VARCHAR(100),
-    email VARCHAR(100),
-    telefoon VARCHAR(20),
-    locatie VARCHAR(100),
-    datum DATE,
-    tijd TIME,
-    aantal INT(11),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    movie_id INT(11),
-    user_id INT(11),
-    bevestigingsnummer VARCHAR(20),
-    FOREIGN KEY (movie_id) REFERENCES movies(id)
-);
